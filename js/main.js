50d350040e9c19396ce155013d1b658ae5cd24f5
lanzimauro <lanzi_mauro@yahoo.it>
2020-04-06 13:12:11 +0200

    mancante caselle vuote

---
+++ js/main.js
@@ -0,0 +1,85 @@
+$(document).ready(function () {
+
+    var htmlGiorno = $('#calendar-template').html();
+    var templateGiorno = Handlebars.compile(htmlGiorno);
+
+    // Stampare il mese di Gennaio 2018
+    // Tramite click stampare il mese successivo
+
+    var dataIniziale = moment('2018-01-01');
+
+    var limiteIniziale = moment('2018-01-01');
+    var limiteFinale = moment('2018-12-31');
+
+
+
+    stampaGiorniMese(dataIniziale); // Inizializzazione Calendario
+    stampaFestivi(dataIniziale);
+    var nomeGiorno = dataIniziale.isoWeekday();
+    console.log(nomeGiorno);
+
+    $('.mese-succ').click(function () {
+        $('.mese-prec').prop('disabled', false);
+        dataIniziale.add(1, 'month');
+        stampaGiorniMese(dataIniziale);
+        stampaFestivi(dataIniziale);
+        var nomeGiorno = dataIniziale.isoWeekday();
+        console.log(nomeGiorno);
+    });
+
+    $('.mese-prec').click(function () {
+        if (dataIniziale.isSameOrBefore(limiteIniziale)) {
+            alert('Hai provato ad hackerarmi!');
+        } else {
+            dataIniziale.subtract(1, 'month');
+            stampaGiorniMese(dataIniziale);
+            stampaFestivi(dataIniziale);
+            if (dataIniziale.isSameOrBefore(limiteIniziale)) {
+                $('.mese-prec').prop('disabled', true);
+            }
+        }
+
+    });
+
+
+
+    function stampaFestivi(variabileMeseCorrente) {
+        $.ajax({
+            url: 'https://flynn.boolean.careers/exercises/api/holidays',
+            method: 'GET',
+            data: {
+                year: variabileMeseCorrente.year(),
+                month: variabileMeseCorrente.month()
+            },
+            success: function (data) {
+                var giorniFestivi = data.response;
+                for (var i = 0; i < giorniFestivi.length; i++) {
+                    var giornoFestivo = giorniFestivi[i];
+                    var nomeFestivo = giornoFestivo.name;
+                    var dataFestivo = giornoFestivo.date;
+                    $('#calendar li[data-day="' + dataFestivo + '"]').addClass('festivo').append(' - ' + nomeFestivo);
+                }
+            }
+        });
+    }
+
+    function stampaGiorniMese(meseDaStampare) {
+        $('#calendar').empty();
+        var standardDay = meseDaStampare.clone();
+        var giorniMese = meseDaStampare.daysInMonth();
+        var nomeMese = meseDaStampare.format('MMMM');
+        $('#nome-mese').text(nomeMese); // Aggiorniamo il nome del mese in top calendar
+        
+        for (var i = 1; i <= giorniMese; i++) {
+            // $('#calendar').append('<li>' + i + ' ' + nomeMese + '</li>');
+            var giornoDaInserire = {
+                day: i + ' ' + nomeMese,
+                dataDay: standardDay.format('YYYY-MM-DD')
+            }
+            var templateFinale = templateGiorno(giornoDaInserire); // Stiamo popolando il template con i dati dell'oggetto
+            $('#calendar').append(templateFinale);
+            standardDay.add(1, 'day');
+        }
+    }
+
+});
